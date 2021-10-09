b76a6b971a6dbf05279466e58577065bab80769c
ajay <15915423+ajaymur91@users.noreply.github.com>
2021-10-09 11:00:08 -0500

    update run_n.sh and calc_FE.sh

--- calc_FE.sh
+++ calc_FE.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
+set -e
+REF='/home/ajay/Documents/opes4/reference'
+Ion1=$1
+Ion2=$2
+HISTO="$REF/$Ion1$Ion2/histo_wall2"
+rm -rf bck.*
+sed '2,10000d' Colvar.data > BIAS2
 #cat << EOF > bw.R
 ##!/bin/Rscript
 #library(stats)
-#C <- read.table('BIAS')
+#C <- read.table('BIAS2')
 #print(bw.nrd0(C\$V2))
 #EOF
-#
+
+sed '2,200d' Colvar.data > BIAS2
 #bw=$(Rscript --vanilla bw.R | awk '{print $2}')
 bw=0.02
 echo $bw
@@ -23,8 +30,8 @@ weights: REWEIGHT_BIAS TEMP=313 ARG=opes.bias,uwall.bias,w1.bias #w2.bias,w3.bia
 
 HISTOGRAM ...
   ARG=di
-  GRID_MIN=0.1
-  GRID_MAX=1.2
+  GRID_MIN=0.001
+  GRID_MAX=2
   GRID_BIN=500
   BANDWIDTH=$bw
   LOGWEIGHTS=weights
@@ -34,48 +41,92 @@ HISTOGRAM ...
 DUMPGRID GRID=hh  FILE=histo FMT=%24.16e
 EOF
 
-sed '2,10000d' Colvar.data > BIAS2
+#sed '2,2d' Colvar.data > BIAS2
+L=$(wc -l Colvar.data)
+echo $L
+sed "2,50000d" Colvar.data | sed -n "1~10 p" > BIAS2
+#cat Colvar.data  > BIAS2
 cat opes.dat | plumed driver --noatoms --plumed /dev/stdin --kt 2.603
 
 cat << 'EOF' > plot.R
 #!/bin/Rscript
-pdf('FE.pdf')
-H <- read.table('histo')
-F <- -log(H$V2)+2*log(H$V1)
-H2 <- read.table('/media/Data/work/gromacs/LiBNMB/MTD_final_all/LiTFSI_S/histo_wall2')
-F2 <- -log(H2$V2)+2*log(H2$V1)
-plot(H$V1,F-min(F),ylim=c(0,15),xlim=c(0,0.6))
-lines(H2$V1,F2-min(F2),col="red")
-dev.off()
-EOF
 
-cat << 'EOF' > plot.R
-#!/bin/Rscript
-pdf('FE.pdf')
-H <- read.table('histo')
-F <- -log(H$V2)+2*log(H$V1)
-L <- which(H$V1>0.4)[1]
-F_L <- F-min(F[1:L])
-Min <- which(F_L[1:L]==min(F_L[1:L]))
-Max <- Min + which(F_L[Min:L]==max(F_L[Min:L]))
-Bar <- F[Max] - F[Min]
-H2 <- read.table('/media/Data/work/gromacs/LiBNMB/MTD_final_all/LiTFSI_S/histo_wall2')
-F2 <- -log(H2$V2)+2*log(H2$V1)
+# Find Minima/Maxima
+####################
+local.min.max <- function(x, dev=mean, plot=TRUE, add.points=FALSE,  ...) {
+  x <- stats::na.omit(x)
+  r <- rle(x) 
+  minima <- which(rep(x=diff(sign(diff(c(-Inf, r$values, -Inf)))) == 2, times=r$lengths))
+  maxima <- which(rep(x=diff(sign(diff(c(-Inf, r$values, -Inf)))) == -2, times=r$lengths)) 
+  if (plot == TRUE) {				 
+    plot(x,type="l", ...)
+    graphics::points(x[minima]~minima,pch=19,col="blue") 
+    graphics::points(x[maxima]~maxima,pch=19,col="red")
+    graphics::abline(h=dev(x, na.rm=TRUE), col="grey")
+    if (add.points == TRUE) graphics::points(x, col="grey")
+    graphics::legend("topleft", legend=c("Minima","Maxima"), pch=c(19,19), 
+                     col=c("blue","red"), bg="white")
+  }
+  return( list(minima=x[minima], maxima=x[maxima],
+               devmin=abs(dev(x) - x[minima]), 
+               devmax=abs(dev(x) - x[maxima])) )
+}
+####################
 
+png('FE.png')
+H <- read.table('histo')
+FE <- -log(H$V2)+2*log(H$V1)
+H2 <- read.table('HISTO')
+FE2 <- -log(H2$V2)+2*log(H2$V1)
+
+# Trim 
+x <- H$V1[is.finite(FE)]
+F <- FE[is.finite(FE)]
+x2 <- H2$V1[is.finite(FE2)]
+F2 <- FE2[is.finite(FE2)]
+
+# Calc barrier
+LMM <- local.min.max(F,plot = FALSE)
+Min <- which(F==LMM$minima[1])
+Max <- which(F==LMM$maxima[2])
+LMM2 <- local.min.max(F2,plot = FALSE)
+if(x[Max] < 0.65){ 
+Bar <- LMM$maxima[2] - LMM$minima[1]
+} else {
+L <- which(x>0.65)[1]
+Bar <- F[L] - LMM$minima[1]
+}
+
+# calc binding energy
+BE <- NA
+L <- which(x>0.65)[1]
+if(length(LMM$minima) > 1){ 
+  BE <- LMM$minima[2] - LMM$minima[1]
+  Min2 <- which(F==LMM$minima[2])
+} else{ 
+    BE <- F[L] - LMM$minima[1]
+    Min2 <- L
+}
+
+# Plot
 par(mar=c(5,5,2,2))
-#plot(H$V1,F-min(F[1:L]),ylim=c(-5*Bar,3*Bar),xlim=c(0.15,0.65),type='l',col="red",ylab="FE (kT)",xlab="r (nm)",cex.lab=1.5,lwd=2,cex.axis=1.2,main="LiTFSI (EC)")
-plot(H$V1,F-min(F[1:L]),ylim=c(-2,10),xlim=c(0.15,0.65),type='l',col="red",ylab="FE (kT)",xlab="r (nm)",cex.lab=1.5,lwd=2,cex.axis=1.2,main="LiTFSI (EC)")
-lines(H2$V1,F2-min(F2[1:L]),lty=2,lwd=2)
+plot(x,F-LMM$minima[1],ylim=c(-10,20),xlim=c(0.15,0.75),type='l',col="red",ylab="FE (kT)",xlab="r (nm)",cex.lab=1.5,lwd=2,cex.axis=1.2,main="PMF (Li-TF)")
+lines(x2,F2 - LMM2$minima[1],lty=2,lwd=2)
 text(0.65,1.5*Bar,"Upper \n Wall",pos=2,cex=2,col="blue")
 abline(v=0.65,lwd=3,col="blue")
 abline(h=0,lty=3)
-legend("topright",c('Bulk','Cluster'),col=c('black','red'),bg="antiquewhite",lty=c(2,1),cex=1.8,lwd=c(3,3))
+legend("bottomright",c('Bulk','Cluster'),col=c('black','red'),bg="antiquewhite",lty=c(2,1),cex=1.8,lwd=c(3,3))
 library(shape)
-shape::Arrows(x0=H$V1[Min],y0=0,x1=H$V1[Min],y1=F_L[Max],code=2,arr.adj=1,lwd=2)
-text(H$V1[Min],Bar,paste0(round(Bar,2)," kT"),pos=3,cex=1.5,col="green3")
+shape::Arrows(x0=x[Min],y0=0,x1=x[Min],y1=Bar,code=2,arr.adj=1,lwd=2)
+text(x[Min],Bar,paste0(round(Bar,2)," kT"),pos=3,cex=1.5,col="green3")
+
+shape::Arrows(x0=x[Min2],y0=0,x1=x[Min2],y1=BE,code=2,arr.adj=1,lwd=2)
+text(x[Min2],BE/2,paste0(round(BE,2)," kT"),pos=4,cex=1.5,col="green3")
+
 write.table(x = Bar,row.names = FALSE,col.names = FALSE,file = 'barrier')
+write.table(x = BE,row.names = FALSE,col.names = FALSE,file = 'bindE')
 dev.off()
 EOF
-
+sed -i "s|HISTO|$HISTO|g" plot.R
 Rscript --vanilla plot.R
 
--- run_n.sh
+++ run_n.sh
@@ -7,55 +7,55 @@
 set -e
 export GMX_MAXBACKUP=-1     # Overwrites
 export PLUMED_MAXBACKUP=-1  # Unlimited backups
+#conda activate nucl
 #export OMP_NUM_THREADS=6
 ###############################
 
 #source gmx_plumed/bin/activate 
-# Edit inputs below
+# Edit inputs (defaults) below
 TEMPERATURE=313
-POFF=${1:-0}
-NTOMP=${2:-3}
-
-#Ion1 
 Ion1=LI
-Ion_q1=1
-Ion2=TFSI
-Ion_q2=-1
-
-#Solvent
+Ion2=TF
 Solv=EC
+NTOMP=3
+Ion_q1=1
+Ion_q2=-1
 Solv_q=0
-
+CA1=LI   #Solute_central_Atom
+CA2=BB   #Solute_central_Atom
+SA21=C4  #Solvent_binding_Atom 
+NSOLV=30
 Tot_q=$(($Ion_q1+$Ion_q2))
-
-#Solute_central_Atom
-CA1=LI
-CA2=S1
-
-#Solvent_binding_Atom 
-SA11=O2
-SA12=O2
-SA21=C4
-SA22=C4
-SA23=C4
-SA24=C4
-
-#Number of Solv
-n1=20
-n2=20
 
 #Inner shell radius (nm)
 R1=0.28
 R2=0.28
 R_SOL=2
-#FUNCTIONAL and BASIS SET
-#FUNCTIONAL=MP2
-#BASIS_SET=aug-cc-pvdz
 
 #Trajectory sampling time (ps) - do not change
 nsteps=50000
-nstepsmd=100000     #dt is set to 0.5 fs in mdp file
-nstepsmtd=10000000 #0
+nstepsmd=500000     #dt is set to 0.5 fs in mdp file
+nstepsmtd=5000000 #0
+
+while getopts c:a:f:n:T:P:N:S: flag
+do
+    case "${flag}" in
+        c) Ion1=${OPTARG};;
+        a) Ion2=${OPTARG};;
+        f) Solv=${OPTARG};;
+        n) NTOMP=${OPTARG};;
+        T) TEMPERATURE=${OPTARG};;
+        P) CA1=${OPTARG};;
+        N) CA2=${OPTARG};;
+        S) SA21=${OPTARG};;
+    esac
+done
+echo "Cation: $Ion1";
+echo "Anion: $Ion2";
+echo "Solv: $Solv";
+echo "NTOMP: $NTOMP";
+echo "TEMPERATURE: $TEMPERATURE";
+
 ###############################
 
 cat << EOF > ion.top
@@ -136,14 +136,19 @@ nstlog                  = 2000       ; update log file every 1.0 ps
 
 ; Parameters describing how to find the neighbors of each atom and how to calculate the interactions
 cutoff-scheme            = verlet    ; Buffered neighbor searching
-rlist                    = 1.5   ;gas ph min (0 means no cutoff)
-rcoulomb                 = 1.5   ;gas ph min (0 means no cutoff)
-rvdw                     = 1.5   ;gas ph min (0 means no cutoff)
+rlist                    = 1.2   ;gas ph min (0 means no cutoff)
+rcoulomb                 = 1.2   ;gas ph min (0 means no cutoff)
+rvdw                     = 1.2   ;gas ph min (0 means no cutoff)
 pbc                      = xyz
-nstlist                  = 10
+;nstlist                  = 10
 ns-type                  = simple
-constraints              = none      ; bonds involving H are constrained
-continuation             = no  ; does the same thing as unconstrained_start
+;constraints              = none      ; bonds involving H are constrained
+;continuation             = no  ; does the same thing as unconstrained_start
+;DispCorr                = EnerPres
+constraint_algorithm    = lincs     ; holonomic constraints
+constraints             = all-bonds   ; bonds involving H are constrained
+lincs_iter              = 1         ; accuracy of LINCS
+lincs_order             = 4         ; also related to accuracy
 
 ;       Vacuum simulations are a special case, in which neighbor lists and cutoffs are basically thrown out.
 ;       All interactions are considered (rlist = rcoulomb = rvdw = 0) and the neighbor list is fixed (nstlist = 0).
@@ -161,15 +166,15 @@ EOF
 
 cat << EOF > min.mdp
 integrator  = steep         ; Algorithm (steep = steepest descent minimization)
-emtol       = 0.001          ; Stop minimization when the maximum force < 1000.0 kJ/mol/nm
-emstep      = 0.001          ; Minimization step size
+emtol       = 0.1          ; Stop minimization when the maximum force < 1000.0 kJ/mol/nm
+emstep      = 0.01          ; Minimization step size
 nsteps      = 50000         ; Maximum number of (minimization) steps to perform
 
 ; Parameters describing how to find the neighbors of each atom and how to calculate the interactions
 ;cutoff-scheme            = verlet    ; Buffered neighbor searching
-;rlist                    = 1.5   ;gas ph min (0 means no cutoff)
-;rcoulomb                 = 1.5   ;gas ph min (0 means no cutoff)
-;rvdw                     = 1.5   ;gas ph min (0 means no cutoff)
+;rlist                    = 1.2   ;gas ph min (0 means no cutoff)
+;rcoulomb                 = 1.2   ;gas ph min (0 means no cutoff)
+;rvdw                     = 1.2   ;gas ph min (0 means no cutoff)
 ;pbc                      = xyz
 ;nstlist                  = 10
 ;ns-type                  = simple
@@ -181,9 +186,9 @@ rlist                    = 0   ;gas ph min (0 means no cutoff)
 rcoulomb                 = 0   ;gas ph min (0 means no cutoff)
 rvdw                     = 0   ;gas ph min (0 means no cutoff)
 pbc                      = no
-nstenergy                = 10
-nstxout                  = 10
-nstlist                  = 0
+;nstenergy                = 10
+;nstxout                  = 10
+;nstlist                  = 10
 ns-type                  = simple
 continuation             = no  ; does the same thing as unconstrained_start
 ;unconstrained_start     = yes ; depricated
@@ -196,14 +201,14 @@ EOF
 
 # Start from Ion.gro and insert waters and generate force-field
 cp ion.top system.top
-gmx insert-molecules -f struct/"$Ion1".gro -ci struct/$Ion2.gro -o Ions.gro -box 1 1 1 -nmol 1 -try 1000 -scale 3 #&> /dev/null
+gmx insert-molecules -f struct/"$Ion1".gro -ci struct/$Ion2.gro -o Ions.gro -box 1.5 1.5 1.5 -nmol 1 -try 1000 -scale 3 #&> /dev/null
 #cat << EOF >> system.top
 #$Ion2   1
 #EOF
 
-gmx insert-molecules -f Ions.gro -ci struct/$Solv.gro -o IonW.gro -box 2 2 2 -nmol $((n1+n2)) -try 1000 -scale 0.1 #&> /dev/null
+gmx insert-molecules -f Ions.gro -ci struct/$Solv.gro -o IonW.gro -box 2 2 2 -nmol $NSOLV -try 1000 -scale 0.1 #&> /dev/null
 cat << EOF >> system.top
-$Solv   $((n1+n2))
+$Solv   $NSOLV
 EOF
 
 ###############################
@@ -211,30 +216,16 @@ EOF
 # Make index and plumed.dat ( to enforce QCT criterion )
 gmx select -f IonW.gro -s IonW.gro -on CA1.ndx -select "atomname $CA1 and resnr 1" &> /dev/null
 gmx select -f IonW.gro -s IonW.gro -on CA2.ndx -select "atomname $CA2 and resnr 2" &> /dev/null
-gmx select -f IonW.gro -s IonW.gro -on SA1.ndx -select "(atomname $SA11 || atomname $SA12) and resnr < $((3+$n1))" &> /dev/null
-gmx select -f IonW.gro -s IonW.gro -on SA2.ndx -select "(atomname $SA21 || atomname $SA22 || atomname $SA23 || atomname $SA24 ) and resnr > $((2+$n1))" &> /dev/null
 gmx select -f IonW.gro -s IonW.gro -on mtd.ndx -select "atomname $SA21 and resnr > 2" &> /dev/null
 
-#cat << EOF > plumed.dat
-#CA1: GROUP NDX_FILE=CA1.ndx # NDX_GROUP=atomname_${CA1}_and_resnr_1
-#SA1: GROUP NDX_FILE=SA1.ndx # NDX_GROUP=atomname_${SA11}_||_and_resnr_<_$((3+$n1))
-#cn1: COORDINATION GROUPA=CA1 GROUPB=SA1 R_0=$(echo $R1+0.12 | bc) NN=12
-#CA2: GROUP NDX_FILE=CA2.ndx # NDX_GROUP=atomname_${CA2}_and_resnr_2
-#SA2: GROUP NDX_FILE=SA2.ndx # NDX_GROUP=atomname_${SA2}_and_resnr_>_$((2+$n1))
-#cn2: COORDINATION GROUPA=CA2 GROUPB=SA2 R_0=$(echo $R2+0.12 | bc) NN=12
-#EOF
-#
-#echo "UPPER_WALLS ARG=cn1 AT=6.1 KAPPA=500 LABEL=UWcn1" >> plumed.dat
-#echo "UPPER_WALLS ARG=cn2 AT=8.1 KAPPA=500 LABEL=UWcn2" >> plumed.dat
-#echo "LOWER_WALLS ARG=cn1 AT=4.9 KAPPA=500 LABEL=LWcn1" >> plumed.dat
-#echo "LOWER_WALLS ARG=cn2 AT=5.9 KAPPA=500 LABEL=LWcn2" >> plumed.dat
-
+Nt=$NSOLV 
 cat << EOF > plumed.dat
 CA1: GROUP NDX_FILE=CA1.ndx # NDX_GROUP=atomname_${CA1}_and_resnr_1
 CA2: GROUP NDX_FILE=CA2.ndx # NDX_GROUP=atomname_${CA2}_and_resnr_2
 com: CENTER ATOMS=CA1,CA2
 SA: GROUP NDX_FILE=mtd.ndx # NDX_GROUP=atomname__and_resnr_<_6
-cn: COORDINATION GROUPA=com GROUPB=SA R_0=1 NN=12
+cn: COORDINATION GROUPA=com GROUPB=SA R_0=$R_SOL NN=12
+LW: LOWER_WALLS ARG=cn AT=${Nt} KAPPA=1000
 
 di: DISTANCE ATOMS=CA1,CA2
 UPPER_WALLS ...
@@ -247,48 +238,29 @@ UPPER_WALLS ...
  LABEL=uwall
 ... UPPER_WALLS
 EOF
-#k=0;
-#for i in $(tail -n 1 SA1.ndx)
-#do
-#k=$(($k+1))
-#echo "d$k: DISTANCE ATOMS=CA1,$i" >> plumed.dat
-#echo -e "UPPER_WALLS ARG=d$k AT=$R1 KAPPA=500 LABEL=w$k" >> plumed.dat
-#cat << EOF >> plumed.dat
-#EOF
-#done
-#
-#for i in $(tail -n 1 SA2.ndx)
-#do
-#k=$(($k+1))
-#echo "d$k: DISTANCE ATOMS=CA2,$i" >> plumed.dat
-#echo -e "UPPER_WALLS ARG=d$k AT=$R2 KAPPA=500 LABEL=w$k" >> plumed.dat
-#cat << EOF >> plumed.dat
-#EOF
-#done
 echo "PRINT ARG=* FILE=COLVAR STRIDE=100" >> plumed.dat
-#echo "PRINT ARG=*.bias FILE=WALL STRIDE=100" >> plumed.dat
 
 ###############################
 
 # Minimize
-echo -e "\n Run Minimization $Ion1-$Ion2 and $((n1+n2)) $Solv \n"
-gmx grompp -f min.mdp -c IonW.gro -p system.top -o min.tpr  #&> /dev/null
+gmx editconf -f IonW.gro -c -box 4 4 4 -o start1.gro #&> /dev/null
+echo -e "\n Run Minimization $Ion1-$Ion2 and $NSOLV $Solv \n"
+gmx grompp -f min.mdp -c start1.gro -p system.top -o min.tpr  #&> /dev/null
 gmx mdrun -deffnm min -nsteps 1000000 -plumed plumed.dat  #&> /dev/null
 
 # Make box bigger (does not really matter - but do it anyway to be safe)
-gmx editconf -f min.gro -c -box 4 4 4 -o start.gro #&> /dev/null
+gmx editconf -f min.gro -c -box 4.786 4.786 4.786 -o start.gro #&> /dev/null
 
 #Md (100 ps)
-echo -e "\n Run MD - $Ion1-$Ion2 - $((n1+n2)) $Solv \n"
+echo -e "\n Run MD - $Ion1-$Ion2 - $NSOLV $Solv \n"
  gmx grompp -f verlet.mdp -c start.gro -p system.top -o md.tpr #&> /dev/null
- gmx mdrun -v -deffnm md -nsteps $nstepsmd -plumed plumed.dat -ntomp $NTOMP #&> /dev/null
+ gmx mdrun -deffnm md -nsteps $nstepsmd -plumed plumed.dat -ntomp $NTOMP #&> /dev/null
 
-#gmx solvate -cp min2.gro -cs struct/ec_equil_1000.gro -p system.top -o solv.gro 
-#gmx select -f solv.gro -s solv.gro -on solv.ndx -select "atomname O2" &> /dev/null
 ###############################
-Nt=$((n1+n2)) 
+Nt=$NSOLV 
 
 cat << EOF > plumed_MTD.dat
+ene: ENERGY
 CA1: GROUP NDX_FILE=CA1.ndx # NDX_GROUP=atomname_${CA1}_and_resnr_1
 CA2: GROUP NDX_FILE=CA2.ndx # NDX_GROUP=atomname_${CA2}_and_resnr_2
 com: CENTER ATOMS=CA1,CA2
@@ -296,79 +268,45 @@ SA: GROUP NDX_FILE=mtd.ndx # NDX_GROUP=atomname__and_resnr_<_6
 cn: COORDINATION GROUPA=com GROUPB=SA R_0=$R_SOL NN=12
 EOF
 
-echo "LOWER_WALLS ARG=cn AT=${Nt} KAPPA=500 LABEL=LW" >> plumed_MTD.dat
-#k=0;
-#for i in $(tail -n 1 SA1.ndx)
-#do
-#k=$(($k+1))
-#echo "d$k: DISTANCE ATOMS=CA1,$i" >> plumed_MTD.dat
-#echo -e "UPPER_WALLS ARG=d$k AT=$R1 KAPPA=500 LABEL=w$k" >> plumed_MTD.dat
-#done
-#
-#for i in $(tail -n 1 SA2.ndx)
-#do
-#k=$(($k+1))
-#echo "d$k: DISTANCE ATOMS=CA2,$i" >> plumed_MTD.dat
-#echo -e "UPPER_WALLS ARG=d$k AT=$R2 KAPPA=500 LABEL=w$k" >> plumed_MTD.dat
-#done 
+echo "LOWER_WALLS ARG=cn AT=${Nt} KAPPA=1000 LABEL=LW" >> plumed_MTD.dat
 
 cat << EOF >> plumed_MTD.dat
 di: DISTANCE ATOMS=CA1,CA2
-
-#METAD ...
-# LABEL=metad
-# ARG=di
-# SIGMA=0.08
-# HEIGHT=1.
-# BIASFACTOR=15
-# TEMP=${TEMPERATURE}
-# PACE=500
-# GRID_MIN=0.0
-# GRID_MAX=1.2
-# GRID_BIN=500
-# CALC_RCT
-# #RCT_USTRIDE=50
-# #REWEIGHTING_NGRID=500
-#... METAD
 
 opes: OPES_METAD ...
   ARG=di
   FILE=Kernels.data
   TEMP=${TEMPERATURE}
-  PACE=100
-  BARRIER=20
-  SIGMA=0.04
-  SIGMA_MIN=0.02
+  PACE=50
+  BARRIER=100
+  SIGMA=0.05
+  SIGMA_MIN=0.0001
   #BIASFACTOR=25
   STATE_WFILE=State.data
   STATE_WSTRIDE=10000
   STORE_STATES
-...
-
-
+ ...
 
 UPPER_WALLS ...
  ARG=di
  AT=0.65
- KAPPA=2000.0
+ KAPPA=200000.0
  EXP=2
  EPS=1
  OFFSET=0.
  LABEL=uwall
 ... UPPER_WALLS
 
-PRINT FMT=%g STRIDE=100 FILE=Colvar.data ARG=di,cn,*.bias,opes.*
-# PRINT ARG=di,cn,*.bias,metad.rbias FILE=BIAS STRIDE=100
+PRINT FMT=%g STRIDE=10 FILE=Colvar.data ARG=di,ene,cn,*.bias,opes.*
 EOF
 
 ###############################
 
 # WT-MTD (1000 ps)
-echo -e "\n Run WT-MTD - $Ion1 - $Ion2 and $((n1+n2)) $Solv \n"
- gmx grompp -f verlet.mdp -c md.gro -p system.top -o mtd.tpr #&> /dev/null
- gmx mdrun -v -deffnm mtd -nsteps $nstepsmtd -plumed plumed_MTD.dat -ntomp $NTOMP #&> /dev/null
+echo -e "\n Run WT-MTD - $Ion1 - $Ion2 and $NSOLV $Solv \n"
+ gmx grompp -f verlet.mdp -c md.gro -p system.top -o mtd.tpr -t md.cpt #&> /dev/null
+ gmx mdrun -deffnm mtd -nsteps $nstepsmtd -plumed plumed_MTD.dat -ntomp $NTOMP #&> /dev/null
 
 ###############################
 rm -rf barrier
-bash calc_FE.sh; xdg-open FE.pdf
- 
+bash calc_FE.sh $Ion1 $Ion2; xdg-open FE.pdf
